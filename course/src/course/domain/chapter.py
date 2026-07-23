5bbc1a65632421bc83657df786245d6f4adc0668
adamjohannes <141582502+adamjohannes@users.noreply.github.com>
2026-07-23 11:49:50 -0300

    feat(course): add a Chapter entity

---
+++ course/src/course/domain/chapter.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from typing import Optional
+from uuid import UUID
+
+from src.course.domain.chapter_body import ChapterBody
+from src.course.domain.exceptions import AlreadyArchivedError, NotArchivedError
+from src.course.domain.title import Title
+
+
+@dataclass
+class Chapter:
+    id: UUID
+    title: Title
+    body: ChapterBody
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    archived_at: Optional[datetime] = None
+
+    def update_title(self, title: Title) -> None:
+        self.title = title
+        self.updated_at = datetime.now(timezone.utc)
+
+    def update_body(self, body: ChapterBody) -> None:
+        self.body = body
+        self.updated_at = datetime.now(timezone.utc)
+
+    def archive(self):
+        if self.is_archived:
+            now = datetime.now(timezone.utc)
+            self.archived_at = now
+            self.updated_at = now
+        raise AlreadyArchivedError()
+
+    def unarchive(self):
+        if not self.is_archived:
+            self.archived_at = None
+            self.updated_at = datetime.now(timezone.utc)
+        raise NotArchivedError()
+
+    @property
+    def is_archived(self) -> bool:
+        return self.archived_at is not None
