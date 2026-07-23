7894a5b7b452e6047962228738616759a2009f48
adamjohannes <141582502+adamjohannes@users.noreply.github.com>
2026-07-23 11:27:44 -0300

    feat(course): add a UserName value object

---
+++ course/src/course/domain/user_name.py
@@ -0,0 +1,30 @@
+import re
+from dataclasses import dataclass
+
+NAME_REGEX = re.compile(r"^[a-zA-Z\s]+$")
+
+class InvalidNameLenghtError(Exception):
+    def __init__(self, message: str = "name must be between 2 and 50 characters") -> None:
+        super().__init__(message)
+
+class InvalidNameCharacterError(Exception):
+    def __init__(self, message: str = "name must contain only alphabetical characters") -> None:
+        super().__init__(message)
+
+
+@dataclass(frozen=True)
+class UserName:
+    value: str
+
+    def __post_init__(self) -> None:
+        clean_value = self.value.strip() if isinstance(self.value, str) else self.value
+        object.__setattr__(self, "value", clean_value)
+
+        if not (2 <= len(self.value) <= 50):
+            raise InvalidNameLenghtError()
+
+        if not NAME_REGEX.match(self.value):
+            raise InvalidNameCharacterError()
+
+    def __str__(self) -> str:
+        return self.value
