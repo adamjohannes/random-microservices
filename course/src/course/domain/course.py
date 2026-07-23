8d92d12525b195e9313157960a759ecdad519027
adamjohannes <141582502+adamjohannes@users.noreply.github.com>
2026-07-23 11:56:10 -0300

    fix(course): pass expected value types to Chapter constructor

--- course/src/course/domain/course.py
+++ course/src/course/domain/course.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 from typing import List, Optional
 from uuid import UUID, uuid4
 from chapter import Chapter
+from src.course.domain.chapter_body import ChapterBody
 from src.course.domain.title import Title
 from src.course.domain.exceptions import AlreadyArchivedError, NotArchivedError
 from src.course.domain.user import User
@@ -19,7 +20,7 @@ class Course:
     updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
     archived_at: Optional[datetime] = None
 
-    def add_chapter(self, title: str, body: str) -> Chapter:
+    def add_chapter(self, title: Title, body: ChapterBody) -> Chapter:
         chapter = Chapter(id=uuid4(), title=title, body=body)
         self.chapters.append(chapter)
         self.updated_at = datetime.now(timezone.utc)
