e0ace89f8afbc75a653f67ac2d76579d6db67895
adamjohannes <141582502+adamjohannes@users.noreply.github.com>
2026-08-06 22:45:04 -0300

    fix(notifications): use sendMail (no TLS/auth) when SMTP credentials are empty, for MailHog

--- notifications/src/Notification/Adapters/SmtpEmail.hs
+++ notifications/src/Notification/Adapters/SmtpEmail.hs
@@ -8,8 +8,8 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ReaderT, ask, runReaderT)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import Network.Mail.Mime (Address (..), Mail, plainPart, simpleMail')
-import Network.Mail.SMTP (sendMailWithLoginTLS)
+import Network.Mail.Mime (Address (..), Mail, simpleMail')
+import Network.Mail.SMTP (sendMail, sendMailWithLoginTLS)
 import Notification.Config (SmtpConfig (..))
 import Notification.Domain.Email (EmailMessage (..))
 import Notification.Ports.EmailSender (EmailSender (..))
@@ -30,12 +30,13 @@ instance EmailSender SmtpEmailM where
                  (Address (Just (smtpFromName cfg)) (smtpFromAddr cfg))
                  (emSubject msg)
                  (TL.fromStrict (emBody msg))
-    result <- liftIO $ try @SomeException $
-      sendMailWithLoginTLS
-        (smtpHost cfg)
-        (T.unpack $ smtpUser cfg)
-        (T.unpack $ smtpPass cfg)
-        mail
+        host = smtpHost cfg
+        user = T.unpack (smtpUser cfg)
+        pass = T.unpack (smtpPass cfg)
+        send = if null user
+                 then sendMail host mail
+                 else sendMailWithLoginTLS host user pass mail
+    result <- liftIO $ try @SomeException send
     pure $ case result of
       Left err -> Left (show err)
       Right _  -> Right ()
